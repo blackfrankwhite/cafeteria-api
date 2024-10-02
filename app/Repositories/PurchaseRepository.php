f63adb008dc4f00581c01cd208f39a50275a7308
guga <gugakhachvani@gmail.com>
2024-10-02 18:19:09 +0400

    purchase records

--- app/Repositories/PurchaseRepository.php
+++ app/Repositories/PurchaseRepository.php
@@ -10,7 +10,14 @@ class PurchaseRepository
 {
    public function getPurchases()
    {
-       return Purchase::all();
+       return Purchase::join('purchase_records', 'purchases.id', 'purchase_records.purchase_id')
+           ->select(
+               'purchases.*', 
+               DB::raw('SUM(purchase_records.price * purchase_records.amount) as total_price'),
+               DB::raw('SUM(purchase_records.amount) as total_amount')
+               )
+           ->groupBy('purchases.id')
+           ->paginate();
    }
 
     public function createPurchase($data)
@@ -38,8 +45,15 @@ class PurchaseRepository
         return DB::table('purchase_records')
             ->join('purchases', 'purchase_records.purchase_id', 'purchases.id')
             ->join('entities', 'purchase_records.entity_id', 'entities.id')
-            ->select('purchase_records.*', 'purchases.title as purchase_title', 'entities.title as entity_title')
+            ->select(
+                'purchase_records.*', 
+                'purchases.title as purchase_title', 
+                'entities.title as entity_title',
+                DB::raw('SUM(purchase_records.price * purchase_records.amount) as total_price'),
+                DB::raw('SUM(purchase_records.amount) as total_amount')
+                )
             ->where('purchase_id', $purchaseID)
+            ->groupBy('purchase_records.id')
             ->get();
     }
 
